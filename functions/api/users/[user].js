b8451114b9bcb5187f9ad0d8bd2e652cafd0e783
BuildTools <unconfigured@null.spigotmc.org>
2023-12-24 00:04:50 -0600

    I don't even care about the message at this point

--- functions/api/users/[user].js
+++ functions/api/users/[user].js
@@ -27,13 +27,13 @@ export async function onRequestPost(context)
         // Go through each variable in the input, check if it exists, and change the user data in the database appropriately.
         // This is to allow you to send a request to change only certain variables without having to send them all.
         let newData = {
-            name: inputData.name === null ? (userData.name === null ? null : userData.name) : inputData.name,
-            email: inputData.email === null ? (userData.email === null ? null : userData.email) : inputData.email
+            name: inputData.name === null ? userData.name === null : inputData.name,
+            email: inputData.email === null ? userData.email === null : inputData.email
         }
 
         await context.env.db.put('user.' + context.params.user, JSON.stringify(newData));
         
-        return new Response(JSON.stringify(inputData) + " " + JSON.stringify(newData) + " " + JSON.stringify(existingUserData) + " " + existingUserData);
+        return new Response(JSON.stringify(inputData) + " " + JSON.stringify(newData) + " " + JSON.stringify(existingUserData) + " " + existingUserData + " " + (inputData.name === null) + (inputData.email === null) + (userData.name === null) + (userData.email === null));
     }catch (e)
     {
         return new Response(e.stack);
