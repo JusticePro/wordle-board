ef1a12a03c7f7aa6e527b306745ad94751eae860
SuperGamersGames <44580811+SuperGamersGames@users.noreply.github.com>
2023-12-23 22:55:36 -0600

    NoReferrer added to links

--- src/App.js
+++ src/App.js
@@ -10,15 +10,15 @@ function App() {
       <p className='App-Title'>WORDLE<br />B O A R D</p>
       <WordleAnswer />
       <p className='Credits'>
-      Front-End Development - <a href='https://github.com/supergamersgames' target="_blank">SuperGamersGames</a>
+      Front-End Development - <a href='https://github.com/supergamersgames' target="_blank" rel="noreferrer">SuperGamersGames</a>
       <br/>
-      Back-End Development - <a href='https://github.com/justicepro' target="_blank">JusticePro</a>
+      Back-End Development - <a href='https://github.com/justicepro' target="_blank" rel="noreferrer">JusticePro</a>
       <br/>
-      Hosted by <a href='https://www.cloudflare.com/' target="_blank">CloudFlare</a>
+      Hosted by <a href='https://www.cloudflare.com/' target="_blank" rel="noreferrer">CloudFlare</a>
       <br/>
       <br/>
-      Plus by arjuazka from <a href="https://thenounproject.com/browse/icons/term/plus/" target="_blank" title="Plus Icons">Noun Project</a> (CC BY 3.0)
-      Crown by Rahmad romadoni from <a href="https://thenounproject.com/browse/icons/term/crown/" target="_blank" title="Crown Icons">Noun Project</a> (CC BY 3.0)
+      Plus by arjuazka from <a href="https://thenounproject.com/browse/icons/term/plus/" rel="noreferrer" target="_blank" title="Plus Icons">Noun Project</a> (CC BY 3.0)
+      Crown by Rahmad romadoni from <a href="https://thenounproject.com/browse/icons/term/crown/" target="_blank" rel="noreferrer" title="Crown Icons">Noun Project</a> (CC BY 3.0)
       </p>
     </div>
   );
