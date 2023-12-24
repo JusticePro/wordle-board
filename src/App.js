578e888b723808b0228931b147be2a4f7fe2df82
SuperGamersGames <44580811+SuperGamersGames@users.noreply.github.com>
2023-12-23 22:47:36 -0600

    Credits

--- src/App.js
+++ src/App.js
@@ -9,6 +9,16 @@ function App() {
     <div className="App">
       <p className='App-Title'>WORDLE<br />B O A R D</p>
       <WordleAnswer />
+      <p className='Credits'>
+        Front-End Development - <a href='https://github.com/supergamersgames' target="_blank">SuperGamersGames</a>
+      <br/>
+        Back-End Development - <a href='https://github.com/justicepro' target="_blank">JusticePro</a>
+      <br/>
+        Hosted by <a href='https://www.cloudflare.com/' target="_blank">CloudFlare</a>
+      <br/>
+      <br/>
+      Plus by arjuazka from <a href="https://thenounproject.com/browse/icons/term/plus/" target="_blank" title="Plus Icons">Noun Project</a> (CC BY 3.0)
+      </p>
     </div>
   );
 }
