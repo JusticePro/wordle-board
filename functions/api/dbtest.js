bee2bd8f78e905e766956a4613d5d800516866a2
BuildTools <unconfigured@null.spigotmc.org>
2023-12-23 22:41:52 -0600

    Add post request for user data

--- functions/api/dbtest.js
+++
@@ -1,5 +0,0 @@
-export async function onRequest(context)
-{
-    const task = await context.env.db.get('user.0');
-    return new Response(task);
-}
--- functions/api/users/[user].js
+++ functions/api/users/[user].js
@@ -1,8 +1,48 @@
-export async function onRequest(context)
+export async function onRequestGet(context)
 {
     const userData = await context.env.db.get('user.' + context.params.user);
-    /*if (userData === null)
-        return Response.json({});*/
+    if (userData === null)
+        return Response.json({});
+    
+    return new Response(userData);
+}
+
+export async function onRequestPost(context)
+{
+    let existingUserData = await context.env.db.get('user.' + context.params.user);
+    let userData;
+
+    try
+    {
+        userData = JSON.parse(existingUserData);
+    }catch (e)
+    {
+        userData =
+        {
+            name: null,
+            email: null,
+        };
+    }
+    
+    let inputData;
+
+    try
+    {
+        inputData = JSON.parse(context.request.body);
+    }catch (e)
+    {
+        return new Response('Input data is not json');
+    }
+
+    // Go through each variable in the input, check if it exists, and change the user data in the database appropriately.
+    // This is to allow you to send a request to change only certain variables without having to send them all.
+    if (inputData.name !== null)
+        userData.name = inputData.name;
+    
+    if (inputData.email !== null)
+        userData.email = inputData.email;
+
+    await context.env.db.put('user.' + context.params.user, userData);
     
     return new Response(userData);
 }
