1052f08ab6414c2ef5eee35d360dcb9db6bf86a9
SuperGamersGames <44580811+SuperGamersGames@users.noreply.github.com>
2023-12-23 21:38:10 -0600

    Tries are now a List option

--- src/components/WordleAnswer.js
+++ src/components/WordleAnswer.js
@@ -71,13 +71,14 @@ function WordleAnswer() {
       <div className="UserContainer" key={user.id}>
         <Gravatar className="Profile" email={user.email} />
         <p>{user.name}</p>
-        <input
-          type="number"
-          defaultValue={1}
-          min={1}
-          max={6}
-          onChange={handleChange(user.id)}
-        />
+        <select name="num_try" id="num_try" onChange={handleChange(user.id)}>
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+          <option value={6}>6</option>
+        </select>
         <p>Points: {user.points}</p>
       </div>
     ));
