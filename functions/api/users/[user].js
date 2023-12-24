668bbb7fc13359d8e09a87f006c442761dbeefea
BuildTools <unconfigured@null.spigotmc.org>
2023-12-23 23:27:27 -0600

    s

--- functions/api/users/[user].js
+++ functions/api/users/[user].js
@@ -24,17 +24,22 @@ export async function onRequestPost(context)
         
         let inputData = await context.request.json();
 
+        let newData = {
+            name: userData.user,
+            email: userData.email
+        }
+
         // Go through each variable in the input, check if it exists, and change the user data in the database appropriately.
         // This is to allow you to send a request to change only certain variables without having to send them all.
         if (inputData.name !== null)
-            userData.name = inputData.name;
+            newData.name = inputData.name;
         
         if (inputData.email !== null)
-            userData.email = inputData.email;
+        newData.email = inputData.email;
 
-        await context.env.db.put('user.' + context.params.user, JSON.stringify(userData));
+        await context.env.db.put('user.' + context.params.user, JSON.stringify(newData));
         
-        return new Response(JSON.stringify(userData) + " " + JSON.stringify(existingUserData) + " " + existingUserData);
+        return new Response(JSON.stringify(newData) + " " + JSON.stringify(existingUserData) + " " + existingUserData);
     }catch (e)
     {
         return new Response(e.stack);
